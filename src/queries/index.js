2c3e94e2abe88dd8aa0c6b2df2f8b63f6cef4560
jpmontoya182 <jpmontoya182@gmail.com>
2019-04-01 22:33:06 -0500

    Query to pedidos

--- src/queries/index.js
+++ src/queries/index.js
@@ -1,52 +1,68 @@
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 
 export const CLIENTES_QUERY = gql`
-	query obtenerClientes($limite: Int, $offset: Int) {
-		obtenerClientes(limite: $limite, offset: $offset) {
-			id
-			nombre
-			apellido
-			empresa
-		}
-		totalClientes
-	}
+  query obtenerClientes($limite: Int, $offset: Int) {
+    obtenerClientes(limite: $limite, offset: $offset) {
+      id
+      nombre
+      apellido
+      empresa
+    }
+    totalClientes
+  }
 `;
 
 export const CLIENTE_QUERY = gql`
-	query ConsultarCliente($id: ID!) {
-		obtenerCliente(id: $id) {
-			id
-			nombre
-			apellido
-			empresa
-			edad
-			emails {
-				email
-			}
-			tipo
-		}
-	}
+  query ConsultarCliente($id: ID!) {
+    obtenerCliente(id: $id) {
+      id
+      nombre
+      apellido
+      empresa
+      edad
+      emails {
+        email
+      }
+      tipo
+    }
+  }
 `;
 
 export const PRODUCTOS_QUERY = gql`
-	query obtenerProductos($limite: Int, $offset: Int, $stock: Boolean) {
-		obtenerProductos(limite: $limite, offset: $offset, stock: $stock) {
-			id
-			nombre
-			stock
-			precio
-		}
-		totalProductos
-	}
+  query obtenerProductos($limite: Int, $offset: Int, $stock: Boolean) {
+    obtenerProductos(limite: $limite, offset: $offset, stock: $stock) {
+      id
+      nombre
+      stock
+      precio
+    }
+    totalProductos
+  }
 `;
 
 export const PRODUCTO_QUERY = gql`
-	query obtenerProducto($id: ID!) {
-		obtenerProducto(id: $id) {
-			id
-			nombre
-			precio
-			stock
-		}
-	}
+  query obtenerProducto($id: ID!) {
+    obtenerProducto(id: $id) {
+      id
+      nombre
+      precio
+      stock
+    }
+  }
+`;
+
+export const OBTENER_PEDIDO = gql`
+  query obtenerPedidos($cliente: String) {
+    obtenerPedidos(cliente: $cliente) {
+      cliente
+      estado
+      id
+      total
+      fecha
+      pedidos {
+        id
+        cantidad
+      }
+    }
+  }
 `;
